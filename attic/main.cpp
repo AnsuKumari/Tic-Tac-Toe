99dd72219d7eba0c91bc31c3c17fbfe332932a04
Ansu Kumari <ansu.kumari@outlook.com>
2016-08-23 14:09:30 +0530

    Create board

--- attic/main.cpp
+++ attic/main.cpp
@@ -1,5 +1,32 @@
 #include<iostream.h>
 #include<conio.h>
 #include<dos.h>
-char s[3][3]={"   ","   ","   "};
+char s[3][3]={"123","456","789"};
 char p1[15],p2[15]="COMPUTER";
+
+void board()
+{
+	clrscr();
+	cout<<"The block # sequence is as follows:-\n";
+	cout<<"				           |           |      "<<"\n";
+	cout<<"				           |           |      "<<"\n";
+	cout<<"				     "<<s[0][0]<<"     |     "<<s[0][1]<<"     |     "<<s[0][2]<<"\n";
+	cout<<"				           |           |      "<<"\n";
+	cout<<"				___________|___________|___________"<<"\n";
+	cout<<"				           |           |      "<<"\n";
+	cout<<"				           |           |      "<<"\n";
+	cout<<"				     "<<s[1][0]<<"     |     "<<s[1][1]<<"     |     "<<s[1][2]<<"\n";
+	cout<<"				           |           |      "<<"\n";
+	cout<<"				___________|___________|___________"<<"\n";
+	cout<<"				           |           |      "<<"\n";
+	cout<<"				           |           |      "<<"\n";
+	cout<<"				     "<<s[2][0]<<"     |     "<<s[2][1]<<"     |     "<<s[2][2]<<"\n";
+	cout<<"				           |           |      "<<"\n";
+	cout<<"				           |           |      "<<"\n\n\n\n\n\n";
+}
+
+void main()
+{
+	board();
+	getch();
+}
